6016dc5100637412037f98a347a104645a3c977e
JunHwan Kim <nefertiri972@gmail.com>
2022-05-14 17:03:03 +0900

    Add using form data
    - 최종적으로 제출된 폼 데이터를 로드하여 출력

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -37,5 +45,13 @@ export class AppComponent {
   onSubmit() {
     console.log(this.signupForm);
     console.log(this.signupForm.value);
+
+    this.submitted = true;
+
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
